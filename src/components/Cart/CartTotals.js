65a34aaac1185cd5cddb0ea43b43c2011eece35e
massodasuki <massodasuki@gmail.com>
2019-03-27 04:31:40 +0800

    second commit

---
+++ src/components/Cart/CartTotals.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import PayPalButton from "./PayPalButton";
+
+export default function CartTotals ({ value }){
+ const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+
+            <Link to="/cart">
+              <button className="btn btn-outline-danger text-upperacase mb-3 px-5"
+              type="button"
+              onClick={() => clearCart()}
+              >
+              clear cart
+              </button>
+            </Link>
+            <h5>
+              <span> subtotal : </span>
+              <strong> $ { cartSubTotal } </strong>
+            </h5>
+            <h5>
+              <span> tax : </span>
+              <strong> $ { cartTax } </strong>
+            </h5>
+            <h5>
+              <span> total : </span>
+              <strong> $ { cartTotal } </strong>
+            </h5>
+
+            {/* Paypal 5.59*/}
+            <PayPalButton
+              total = { cartTotal }
+              clearCart = { clearCart }
+              history = { history }>
+
+            </PayPalButton>
+
+
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+
+  );
+
+
+}
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info: "Lorem ipsum dolor amate offal butcher quinoa sustainable gastropuc, extually echo pub green",
+    inCart: false,
+    count: 0,
+    total: 0
+
+  },
+
+  {
+    id: 2,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+    "Lorem ipsum dolor amate offal butcher quinoa sustainable gastropuc, extually echo pub green number 2",
+    inCart: false,
+    count: 0,
+    total: 0
+
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Bag Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+  "Lorem ipsum dolor amet offal butcher quinoa sustainable gustable",
+  inCart:false,
+  count: 0,
+  total:0
+};
